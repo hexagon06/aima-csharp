81332dab5fb66067265b38f3b82ca672a13848c7
hexagon06 <jasper.saaltink@gmail.com>
2017-05-17 16:10:41 +0200

    namespace aima.core.search.framework.qsearch

--- aima-csharp/search/framework/qsearch/GraphSearchBFS.cs
+++ aima-csharp/search/framework/qsearch/GraphSearchBFS.cs
@@ -5,102 +5,104 @@ using aima.core.search.framework.problem;
 
 namespace aima.core.search.framework.qsearch
 {
-    /**
-     * Artificial Intelligence A Modern Approach (3rd Edition): Figure 3.7, page 77.
-     * <br>
-     * 
-     * <pre>
-     * function GRAPH-SEARCH(problem) returns a solution, or failure
-     *   initialize the frontier using the initial state of problem
-     *   initialize the explored set to be empty
-     *   loop do
-     *     if the frontier is empty then return failure
-     *     choose a leaf node and remove it from the frontier
-     *     if the node contains a goal state then return the corresponding solution
-     *     add the node to the explored set
-     *     expand the chosen node, adding the resulting nodes to the frontier
-     *       only if not in the frontier or explored set
-     * </pre>
-     * 
-     * Figure 3.7 An informal description of the general graph-search algorithm.
-     * <br>
-     * This implementation is based on the template method
-     * {@link #search(Problem, Queue)} from superclass {@link QueueSearch} and
-     * provides implementations for the needed primitive operations. It is the most
-     * efficient variant of graph search for breadth first search. But don't expect
-     * shortest paths in combination with priority queue frontiers.
-     * 
-     * @author Ravi Mohan
-     * @author Ciaran O'Reilly
-     * @author Ruediger Lunde
-     */
+    /// <summary>
+    /// This implementation is based on the template method
+    /// <see cref="aima.core.search.framework.qsearch.QueueSearch.Search(Problem, Queue{Node})"/>
+    /// and provides implementations for the needed primitive operations.It is the most
+    /// efficient variant of graph search for breadth first search.But don't expect
+    /// shortest paths in combination with priority queue frontiers.
+    /// </summary>
+    /// <remarks>
+    /// Artificial Intelligence A Modern Approach (3rd Edition): Figure 3.7, page 77.
+    /// function GRAPH-SEARCH(problem) returns a solution, or failure
+    ///     initialize the frontier using the initial state of problem
+    ///     initialize the explored set to be empty
+    ///     loop do
+    ///         if the frontier is empty then return failure
+    ///         choose a leaf node and remove it from the frontier
+    ///         if the node contains a goal state then return the corresponding solution
+    ///         add the node to the explored set
+    ///         expand the chosen node, adding the resulting nodes to the frontier
+    ///             only if not in the frontier or explored set
+    ///             
+    /// Figure 3.7 An informal description of the general graph-search algorithm.
+    /// </remarks>
+    /// <author>Ravi Mohan</author>
+    /// <author>Ciaran O'Reilly</author>
+    /// <author>Ruediger Lunde</author>
     public class GraphSearchBFS : QueueSearch
     {
-	private HashSet<object> explored = new HashSet<object>();
-	private HashSet<object> frontierStates = new HashSet<object>();
+        private HashSet<object> explored = new HashSet<object>();
+        private HashSet<object> frontierStates = new HashSet<object>();
 
-	public GraphSearchBFS() : this(new NodeExpander())
-	{
+        public GraphSearchBFS() : this(new NodeExpander())
+        {
 
-	}
+        }
 
-	public GraphSearchBFS(NodeExpander nodeExpander) : base(nodeExpander)
-	{
+        public GraphSearchBFS(NodeExpander nodeExpander) : base(nodeExpander)
+        {
 
-	}
+        }
 
-	/**
-	 * Clears the set of explored states and calls the search implementation of
-	 * <code>QueSearch</code>
-	 */
-	public override List<Action> search(Problem problem, Queue<Node> frontier)
-	{
-	    // Initialize the explored set to be empty
-	    explored.Clear();
-	    frontierStates.Clear();
-	    return base.search(problem, frontier);
-	}
+        /// <summary>
+        /// Clears the set of explored states and calls the search implementation of
+        /// <see cref="aima.core.search.framework.qsearch.QueueSearch.Search(Problem, Queue{Node})"/>
+        /// </summary>
+        /// <param name="problem">the search problem</param>
+        /// <param name="frontier">the collection of nodes that are waiting to be expanded</param>
+        /// <returns>
+        /// a list of actions to the goal if the goal was found, a list
+    	/// containing a single NoOp Action if already at the goal, or an
+        /// empty list if the goal could not be found.</returns>
+        public override List<Action> Search(Problem problem, Queue<Node> frontier)
+        {
+            // Initialize the explored set to be empty
+            explored.Clear();
+            frontierStates.Clear();
+            return base.Search(problem, frontier);
+        }
 
-	/**
-	 * Inserts the node at the tail of the frontier if the corresponding state
-	 * is not already a frontier state and was not yet explored.
-	 */
-	protected override void addToFrontier(Node node)
-	{
-	    if (!explored.Contains(node.getState()) && !frontierStates.Contains(node.getState()))
-	    {
-		frontier.Enqueue(node);
-		frontierStates.Add(node.getState());
-		updateMetrics(frontier.Count);
-	    }
-	}
+        /// <summary>
+        /// Inserts the node at the tail of the frontier if the corresponding state
+        /// is not already a frontier state and was not yet explored.
+        /// </summary>
+        /// <param name="node"></param>
+        protected override void addToFrontier(Node node)
+        {
+            if (!explored.Contains(node.getState()) && !frontierStates.Contains(node.getState()))
+            {
+                frontier.Enqueue(node);
+                frontierStates.Add(node.getState());
+                updateMetrics(frontier.Count);
+            }
+        }
 
-	/**
-	 * Removes the node at the head of the frontier, adds the corresponding
-	 * state to the explored set, and returns the node.
-	 * 
-	 * @return the node at the head of the frontier.
-	 */
-	protected override Node removeFromFrontier()
-	{
-	    Node result = frontier.Dequeue();
-	    explored.Add(result.getState());
-	    frontierStates.Remove(result.getState());
-	    updateMetrics(frontier.Count);
-	    return result;
-	}
+        /// <summary>
+        /// Removes the node at the head of the frontier, adds the corresponding
+        /// state to the explored set, and returns the node.
+        /// </summary>
+        /// <returns>the node at the head of the frontier.</returns>
+        protected override Node removeFromFrontier()
+        {
+            Node result = frontier.Dequeue();
+            explored.Add(result.getState());
+            frontierStates.Remove(result.getState());
+            updateMetrics(frontier.Count);
+            return result;
+        }
 
-	/**
-	 * Checks whether there are still some nodes left.
-	 */
-	protected override bool isFrontierEmpty()
-	{
-	    if (frontier.Count == 0)
-	    {
-		return true;
-	    }
-	    else
-		return false;
-	}
+        /// <summary>
+        /// Checks whether there are still some nodes left.
+        /// </summary>
+        protected override bool isFrontierEmpty()
+        {
+            if (frontier.Count == 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
     }
 }
--- aima-csharp/search/framework/qsearch/QueueSearch.cs
+++ aima-csharp/search/framework/qsearch/QueueSearch.cs
@@ -6,21 +6,23 @@ using aima.core.search.framework.problem;
 
 namespace aima.core.search.framework.qsearch
 {
-    /**
-     * Base class for queue-based search implementations, especially for {@link TreeSearch},
-     * {@link GraphSearch}, and {@link BidirectionalSearch}.
-     * 
-     * @author Ravi Mohan
-     * @author Ciaran O'Reilly
-     * @author Mike Stampone
-     * @author Ruediger Lunde
-     */
-     public abstract class QueueSearch
+    /// <summary>
+    /// Base class for queue-based search implementations, especially for 
+    /// <see cref="TreeSearch"/>,
+    /// <see cref="GraphSearch"/>
+    /// and 
+    /// <see cref="GraphSearchBFS"/>
+    /// </summary>
+    /// <author>Ravi Mohan</author>
+    /// <author>Ciaran O'Reilly</author>
+    /// <author>Mike Stampone</author>
+    /// <author>Ruediger Lunde</author>
+    public abstract class QueueSearch
     {
         public const System.String METRIC_NODES_EXPANDED = "nodesExpanded";
         public const System.String METRIC_QUEUE_SIZE = "queueSize";
-	public const System.String METRIC_MAX_QUEUE_SIZE = "maxQueueSize";
-	public const System.String METRIC_PATH_COST = "pathCost";
+        public const System.String METRIC_MAX_QUEUE_SIZE = "maxQueueSize";
+        public const System.String METRIC_PATH_COST = "pathCost";
 
         protected readonly NodeExpander nodeExpander;
         protected Queue<Node> frontier;
@@ -37,24 +39,24 @@ namespace aima.core.search.framework.qsearch
             return nodeExpander;
         }
 
-        /**
-	 * Returns a list of actions to the goal if the goal was found, a list
-	 * containing a single NoOp Action if already at the goal, or an empty list
-    	 * if the goal could not be found. This template method provides a base for
-    	 * tree and graph search implementations. It can be customized by overriding
-    	 * some primitive operations, especially {@link #addToFrontier(Node)},
-    	 * {@link #removeFromFrontier()}, and {@link #isFrontierEmpty()}.
-    	 * 
-    	 * @param problem
-    	 *            the search problem
-    	 * @param frontier
-    	 *            the collection of nodes that are waiting to be expanded
-    	 * 
-    	 * @return a list of actions to the goal if the goal was found, a list
-    	 *         containing a single NoOp Action if already at the goal, or an
-    	 *         empty list if the goal could not be found.
-    	 */
-         public virtual List<Action> search(Problem problem, Queue<Node> frontier)
+        /// <summary>
+        /// Returns a list of actions to the goal if the goal was found, a list
+        /// containing a single NoOp Action if already at the goal, or an empty list
+        /// if the goal could not be found.This template method provides a base for
+    	/// tree and graph search implementations.It can be customized by overriding
+    	/// some primitive operations, especially 
+        /// <see cref="addToFrontier"/>,
+        /// <see cref="removeFromFrontier"/>
+        /// and
+        /// <see cref="isFrontierEmpty"/>
+        /// </summary>
+        /// <param name="problem">the search problem</param>
+        /// <param name="frontier">the collection of nodes that are waiting to be expanded</param>
+        /// <returns>
+        /// a list of actions to the goal if the goal was found, a list
+    	/// containing a single NoOp Action if already at the goal, or an
+        /// empty list if the goal could not be found.</returns>
+        public virtual List<Action> Search(Problem problem, Queue<Node> frontier)
         {
             this.frontier = frontier;
             clearInstrumentation();
@@ -62,13 +64,13 @@ namespace aima.core.search.framework.qsearch
             Node root = nodeExpander.createRootNode(problem.GetInitialState());
             if (earlyGoalCheck)
             {
-                if(SearchUtils.isGoalState(problem, root))
+                if (SearchUtils.isGoalState(problem, root))
                 {
                     return getSolution(root);
                 }
             }
             addToFrontier(root);
-            while(!(frontier.Count == 0))
+            while (!(frontier.Count == 0))
             {
                 // choose a leaf node and remove it from the frontier
                 Node nodeToExpand = removeFromFrontier();
@@ -78,70 +80,70 @@ namespace aima.core.search.framework.qsearch
                 {
                     // if the node contains a goal state then return the
                     // corresponding solution
-                    if(SearchUtils.isGoalState(problem, nodeToExpand))
+                    if (SearchUtils.isGoalState(problem, nodeToExpand))
                     {
                         return getSolution(nodeToExpand);
                     }
                 }
                 // expand the chosen node, adding the resulting nodes to the
                 // frontier
-                foreach(Node successor in nodeExpander.expand(nodeToExpand, problem))
+                foreach (Node successor in nodeExpander.expand(nodeToExpand, problem))
                 {
                     if (earlyGoalCheck)
                     {
-                        if(SearchUtils.isGoalState(problem, successor))
+                        if (SearchUtils.isGoalState(problem, successor))
                         {
                             return getSolution(successor);
                         }
                     }
                     addToFrontier(successor)
-;                }
+;
+                }
             }
             // if the frontier is empty then return failure
             return SearchUtils.failure();
         }
-        /**
-	 * Primitive operation which inserts the node at the tail of the frontier.
-	 */
+
+        /// <summary>
+        /// Primitive operation which inserts the node at the tail of the frontier.
+        /// </summary>
         protected abstract void addToFrontier(Node node);
 
-        /**
-	 * Primitive operation which removes and returns the node at the head of the
-	 * frontier.
-	 * 
-	 * @return the node at the head of the frontier.
-	 */
+        /// <summary>
+        /// Primitive operation which removes and returns the node at the head of the frontier.
+        /// </summary>
+        /// <returns>the node at the head of the frontier.</returns>
         protected abstract Node removeFromFrontier();
-
-        /**
-	 * Primitive operation which checks whether the frontier contains not yet
-	 * expanded nodes.
-	 */
+        
+        /// <summary>
+        /// Primitive operation which checks whether the frontier contains not yet
+        /// expanded nodes.
+        /// </summary>
         protected abstract bool isFrontierEmpty();
-
-        /**
-	 * Enables optimization for FIFO queue based search, especially breadth
-	 * first search.
-	 * 
-	 * @param state
-	 */
+        
+        /// <summary>
+        /// Enables optimization for FIFO queue based search, especially breadth
+        /// first search.
+        /// </summary>
+        /// <param name="state"></param>
         public void setEarlyGoalCheck(bool state)
         {
             this.earlyGoalCheck = state;
         }
 
-        /**
-	 * Returns all the search metrics.
-	 */
+        /// <summary>
+        /// Returns all the search metrics.
+        /// </summary>
+        /// <returns>all the search metrics.</returns>
         public virtual Metrics getMetrics()
         {
             metrics.set(METRIC_NODES_EXPANDED, nodeExpander.getNumOfExpandCalls());
             return metrics;
         }
 
-        /**
-	 * Sets all metrics to zero.
-	 */
+        /// <summary>
+        /// Sets all metrics to zero.
+        /// </summary>
         public void clearInstrumentation()
         {
             nodeExpander.resetCounter();
